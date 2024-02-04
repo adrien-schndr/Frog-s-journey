8f1e299f6c2038b1cb8e1c1d9a240b03f735a810
Adrien Schneider <adrien.schneider-noury@netc.fr>
2024-02-04 22:09:30 +0100

    Started working on scrolling objects

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import pygame
+import pygame, math
 from pygame.locals import *
 
 
 class Frog(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        skin_frog = "images/green_frog.png"
-        skin_frog = pygame.image.load(skin_frog).convert_alpha()
+        skin_frog = pygame.image.load("images/green_frog.png").convert_alpha()
         skin_frog = pygame.transform.scale(skin_frog, (100, 100))
         self.image = pygame.Surface((100, 100), pygame.SRCALPHA)
         self.rect = self.image.get_rect()
@@ -30,23 +29,49 @@ class Frog(pygame.sprite.Sprite):
                 self.rect.y += vitesse
 
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        skin_obstacle = pygame.image.load("images/train.png").convert_alpha()
+        skin_obstacle = pygame.transform.scale(skin_obstacle, (800, 100))
+        self.image = pygame.Surface((800, 100), pygame.SRCALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.center = (400, 50)
+        self.image.blit(skin_obstacle, (self.rect.x, self.rect.y))
+
+
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080), pygame.SCALED | pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 joueur = Frog()
+voiture = Obstacle()
 
-background_png = "images/background.png"
-background_png = pygame.image.load(background_png).convert_alpha()
+# Chargement du background
+background_png = pygame.image.load("images/background.png").convert_alpha()
+scroll = 0
+tiles = math.ceil(1080 / background_png.get_height()) + 1
 
 running = True
 while running:
+    # Rafraichissement fen^tre
     joueur.deplacer_joueur()
-    screen.blit(background_png, (0, 0))
+    pygame.sprite.Group(voiture).draw(screen)
     pygame.sprite.Group(joueur).draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
+    # Déplacement background
+    i = 0
+    while i < tiles:
+        screen.blit(background_png, (0, background_png.get_height() * i + scroll))
+        voiture.rect.x += scroll/2
+        i += 1
+    scroll -= 0.75
+    if abs(scroll) > background_png.get_height():
+        scroll = 0
+
+    # Quitter le jeu
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
