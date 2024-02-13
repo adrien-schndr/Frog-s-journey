aefd7dd6734e6c25e71331d465529bad128522ed
Adrien Schneider <adrien.schneider-noury@netc.fr>
2024-02-13 17:46:23 +0100

    Added story elements

--- constantes.py
+++ constantes.py
@@ -15,3 +15,5 @@ for image in range(0, 18):
     wooden_log_textures.append(texture)
 
 menu_background = "images/menu_background.png"
+
+story = "Ah voyageur intrépide, ribbit bienvenue dans ces contrées sylvestres. Je suis Layton, Chef de la Guilde, gardien de ces marais ancestraux. J’entrevois dans vos yeux une lueur de détermination et de bravoure ribbit. Je vous confie une quête d’une importance capitale, même si vous ne l’avez peut être pas encore souhaitée. Vous êtes désigné pour récupérer les dix épées légendaires, symbole de pouvoir et de savoir ancien. Je vous servirais de guide n’ayez crainte ribbit. Il est maintenant l’heure pour vous d’accomplir votre destinée. Je vous souhaite bonne chance voyageur ribbit."
