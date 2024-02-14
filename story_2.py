ef2e63ea678630fd6c6baa7ed990972bd78d357e
Adrien Schneider <adrien.schneider-noury@netc.fr>
2024-02-14 11:38:26 +0100

    Updated story files

--- story_2.py
+++ story_2.py
@@ -10,17 +10,25 @@ TITLE_FONT = pygame.freetype.Font("story_font.otf", 40)
 
 title = """LAYTON"""
 texts = [
-    """Ah voyageur intrépide, *ribbit* bienvenue dans ces contrées sylvestres. Je suis Layton,""",
-    """Chef de la Guilde, gardien de ces marais ancestraux. J’entrevois dans vos yeux une lueur""",
-    """de détermination et de bravoure *ribbit*. Je vous confie une quête d’une importance""",
-    """capitale, même si vous ne l’avez peut être pas encore souhaitée. Vous êtes désigné pour""",
-    """récupérer les dix épées légendaires, symbole de pouvoir et de savoir ancien. Je vous""",
-    """servirais de guide n’ayez crainte *ribbit*. Il est maintenant l’heure pour vous """,
-    """d’accomplir votre destinée. Je vous souhaite bonne chance voyageur *ribbit*."""
+    """Bienvenue dans la première étape de votre périple, voyageur. Vous êtes désormais sur""",
+    """le point de vous aventurer dans les profondeurs de notre royaume, où les défis et""",
+    """les mystères abondent. Pour trouver la première des dix épées légendaires, vous devrez """,
+    """traverser ce niveau avec diligence et habileté.""",
+    """Utilisez les touches fléchées de votre clavier pour vous déplacer à travers ce""",
+    """dédale de dangers. Le temps presse, donc ne tardez pas trop. Chaque seconde écoulée""",
+    """vous rapproche de l'échec.""",
+    """Attention aux obstacles qui jonchent votre chemin. Évitez-les avec agilité pour ne""",
+    """pas compromettre votre progression. Seuls les plus habiles et les plus attentifs""",
+    """réussiront à atteindre l'épée convoitée.""",
+    """N'oubliez pas que votre quête est chronométrée. Le temps file comme le vent, et chaque""",
+    """seconde compte. Soyez rapide, mais ne négligez pas la prudence.""",
+    """Une fois que vous aurez atteint l'épée, votre aventure ne fera que commencer. Mais""",
+    """pour cela, vous devez d'abord surmonter ce premier défi avec succès. Allez, brave""",
+    """voyageur, que la chance soit avec vous."""
 ]
 
 # Constantes
-menu = "images/story_screen.png"
+menu = "images/story_screen_2.png"
 
 
 def creation_fenetre():
@@ -60,10 +68,10 @@ def creation_fenetre():
             if event.type == MOUSEBUTTONDOWN and event.button == 1:  # Vérifier le clic de souris
                 # Récupérer les coordonnées du clic
                 clic_x, clic_y = event.pos
-                if 1349 < clic_x < 1407 and 854 < clic_y < 959:
+                print(clic_x, clic_y)
+                if 1313 < clic_x < 1366 and 828 < clic_y < 941:
                     running = False
                     import select_lvl
     pygame.quit()
-
 
 creation_fenetre()
